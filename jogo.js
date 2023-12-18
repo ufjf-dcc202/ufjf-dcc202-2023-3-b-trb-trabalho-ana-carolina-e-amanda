85ae110e37ce7fcd2224d8de1743382413fe4f53
Amanda Marques <131484602+amandaamarques@users.noreply.github.com>
2023-12-18 11:33:51 -0300

    JS - Inicializamos as variáveis e criamos a função para rolar o dado

--- jogo.js
+++ jogo.js
@@ -1,7 +1,19 @@
-function lancarDado(){
-    let a = 1 + (6 * Math.random());
-    let b = Math.floor(a);
-    console.log(b);
-};
+// Inicializando as variáveis
+let jogadorAtual = 1;
+let tabela1 = [], tabela2 = [];
 
-lancarDado();
+for (let i = 0; i < 3; i++) {
+    tabela1[i] = [];
+    tabela2[i] = [];
+    for (let j = 0; j < 3; j++) {
+        tabela1[i][j] = undefined;
+        tabela2[i][j] = undefined;
+    }
+}
+let vezTabela1 = true;
+let dado = null;
+
+// Função para rolar o dado
+function rolarDado() {
+    return Math.floor(Math.random() * 6) + 1;
+}
